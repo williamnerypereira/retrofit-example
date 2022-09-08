13160d5ad5bdaa3543befcf2b2902a5820962cb1
William Nery <william.nery.pereira@gmail.com>
2022-09-08 15:20:42 -0300

    Criação do projeto

---
+++ app/src/main/java/com/williamnery/retrofitexample/MainActivity.kt
@@ -0,0 +1,22 @@
+package com.williamnery.retrofitexample
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
+
+// https://jsonplaceholder.typicode.com/posts
+
+/*
+*   {
+    "userId": 1,
+    "id": 1,
+    "title": "sunt aut facere repellat provident occaecati excepturi optio reprehenderit",
+    "body": "quia et suscipit\nsuscipit recusandae consequuntur expedita et cum\nreprehenderit molestiae ut ut quas totam\nnostrum rerum est autem sunt rem eveniet architecto"
+  },
+* */
